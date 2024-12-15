d93a96848d6ec09710300ff2fee9ca5942034568
Jeevan S M <jeevansm@Jeevans-MacBook-Air.local>
2024-12-16 00:02:05 +0530

    Initial commit

---
+++ lib/mail.ts
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+
+export async function sendMail({to , name , subject , body }:
+    {to: string , name: string , subject: string , body : string}) {
+    const {SMTP_EMAIL , SMTP_PASSWARD} =process.env;
+    
+    const transport = nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user:SMTP_EMAIL,
+            pass:SMTP_PASSWARD,
+        },
+    });
+    try{
+         const testResult =await transport.verify();
+         console.log(testResult);
+    } catch(error){
+        console.log(error)
+        return ;
+    }
+
+    try{
+        const sendResult = await transport.sendMail({
+            from: SMTP_EMAIL , to , subject , html:body
+        })
+        console.log(sendResult);
+    }
+    catch(error){
+       console.log(error)
+    }
+
+
+}
